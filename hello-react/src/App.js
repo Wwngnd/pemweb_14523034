f146fe2514cc1917189982566555a4a2fcfd27d5
Wwngnd <wawangunaadii@gmail.com>
2025-03-21 20:28:44 -0700

    pertemuan 2

---
+++ hello-react/src/App.js
@@ -0,0 +1,16 @@
+// import libraries
+import React from 'react';
+import Header from './Header';
+
+//create component
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p><b>BAMBANG GANTENG</b></p>
+      <Header />
+    </div>
+  );
+};
+//export default
+export default App;
---
+++ hello-react/src/Header.js
@@ -0,0 +1,15 @@
+// import libraries
+import React from 'react';
+
+//membuat component dengan class
+class Header extends React.Component{
+    render(){
+      return(
+        <div>
+          <h2>Makanan Khas Indonesia</h2>
+        </div>
+      );
+    }
+  }
+
+export default Header
